39ded0085b25f553c0db8d149201dac11cd78b7a
medingee <43713964+medingee@users.noreply.github.com>
2022-09-23 14:32:00 -0700

    QP

---
+++ hello.rs
@@ -0,0 +1,23 @@
+// This is a comment, and is ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+    _immutable_binding += 1;
+    // FIXME ^ Comment out this line
+}
